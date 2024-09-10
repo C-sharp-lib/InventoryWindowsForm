da76096029688f85cdf833aea487ddb117c6e6ad
Kevin Stradtman <stradtkt22@gmail.com>
2024-09-10 05:38:40 -0400

    Inventory Assignment

---
+++ InvItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    public class InvItem
+    {
+        public int ItemNo { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public InvItem() { }
+        public InvItem(int itemNo, string description, decimal price)
+        {
+            this.ItemNo = itemNo;
+            this.Description = description;
+            this.Price = price;
+        }
+        
+        public string GetDisplayText(string sep)
+        {
+            return $"{ItemNo}{sep}{Description}{sep}{Price}";
+        }
+    }
+}
---
+++ frmNewItem.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace InventoryMaintenance
+{
+    partial class frmNewItem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new System.Windows.Forms.Label();
+            txtItemNo = new System.Windows.Forms.TextBox();
+            label2 = new System.Windows.Forms.Label();
+            txtDescription = new System.Windows.Forms.TextBox();
+            label3 = new System.Windows.Forms.Label();
+            txtPrice = new System.Windows.Forms.TextBox();
+            btnSave = new System.Windows.Forms.Button();
+            btnCancel = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(18, 16);
+            label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(51, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Item no:";
+            // 
+            // txtItemNo
+            // 
+            txtItemNo.Location = new System.Drawing.Point(96, 14);
+            txtItemNo.Margin = new System.Windows.Forms.Padding(2);
+            txtItemNo.Name = "txtItemNo";
+            txtItemNo.Size = new System.Drawing.Size(106, 23);
+            txtItemNo.TabIndex = 1;
+            txtItemNo.Tag = "Item no";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(18, 48);
+            label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(70, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Description:";
+            // 
+            // txtDescription
+            // 
+            txtDescription.Location = new System.Drawing.Point(96, 46);
+            txtDescription.Margin = new System.Windows.Forms.Padding(2);
+            txtDescription.Name = "txtDescription";
+            txtDescription.Size = new System.Drawing.Size(257, 23);
+            txtDescription.TabIndex = 3;
+            txtDescription.Tag = "Description";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(18, 80);
+            label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(36, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Price:";
+            // 
+            // txtPrice
+            // 
+            txtPrice.Location = new System.Drawing.Point(96, 78);
+            txtPrice.Margin = new System.Windows.Forms.Padding(2);
+            txtPrice.Name = "txtPrice";
+            txtPrice.Size = new System.Drawing.Size(106, 23);
+            txtPrice.TabIndex = 5;
+            txtPrice.Tag = "Price";
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new System.Drawing.Point(96, 119);
+            btnSave.Margin = new System.Windows.Forms.Padding(2);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new System.Drawing.Size(78, 25);
+            btnSave.TabIndex = 6;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new System.Drawing.Point(206, 119);
+            btnCancel.Margin = new System.Windows.Forms.Padding(2);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new System.Drawing.Size(78, 25);
+            btnCancel.TabIndex = 7;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // frmNewItem
+            // 
+            AcceptButton = btnSave;
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            CancelButton = btnCancel;
+            ClientSize = new System.Drawing.Size(372, 158);
+            ControlBox = false;
+            Controls.Add(btnCancel);
+            Controls.Add(btnSave);
+            Controls.Add(txtPrice);
+            Controls.Add(label3);
+            Controls.Add(txtDescription);
+            Controls.Add(label2);
+            Controls.Add(txtItemNo);
+            Controls.Add(label1);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            Margin = new System.Windows.Forms.Padding(2);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmNewItem";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "New Inventory Item";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtItemNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
